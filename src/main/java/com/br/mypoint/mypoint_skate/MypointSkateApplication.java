c4a95fd6c91ead9c4953bdc755130ea1fe6469c6
Ceratt1 <gabrielceratticabral@gmail.com.br>
2025-03-30 17:37:19 -0300

    initialize Spring Boot project with Maven setup and basic application structure

---
+++ src/main/java/com/br/mypoint/mypoint_skate/MypointSkateApplication.java
@@ -0,0 +1,13 @@
+package com.br.mypoint.mypoint_skate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MypointSkateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MypointSkateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/mypoint/mypoint_skate/MypointSkateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.mypoint.mypoint_skate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MypointSkateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
